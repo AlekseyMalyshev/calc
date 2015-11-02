1c6be26040a3aff1c64fef345b5ad59c572d7e10
Aleksey Malyshev <aleksey.malyshev@mail.ru>
2015-11-02 01:29:36 -0800

    rewrote funcionality

--- js/script.js
+++ js/script.js
@@ -4,187 +4,142 @@
  * Date created Oct 29, 2015
  */
 
+'use strict';
+
 (function app() {
-  'use strict';
-
-  let mem = 0;
-  let $mem = $('td#5');
-
-  let resetBuf = false;
-  let buf = 0;
-  let sep = 0;
-  let $buf = $('th#buf');
-
-  let stack = [];
-  let stackCp = [];
-
-  let prevNum = () => {
-    for (let i = stack.length - 1; i >= 0; --i) {
-      let res = parseInt(stack[i]);
-      if (!isNaN(res)) {
-        return res;
-      }
-    }
-    return 1;
-  }
-
-  let clearBuf = () => {
-    buf = 0;
-    sep = 0;
-    $buf.text(buf);
-  }
-
-  let clearOpButtons = () => {
-    $('td#9').removeClass('pushed');
-    $('td#19').removeClass('pushed');
-    $('td#29').removeClass('pushed');
-    $('td#39').removeClass('pushed');
-  }
-
-  let processOp = (op, $btn) => {
-    if (stack.length > 0 &&
-        !!~'/*-+'.indexOf(stack[stack.length - 1])) {
-      stack.pop();
-    }
-    else {
-      stack.push(buf);
-    }
-    stack.push(op);
-    buf = 0;
-    sep = 0;
-    clearOpButtons();
-    if ($btn !== undefined) {
-      $btn.addClass('pushed');
-    }
-  }
-
-  let processStack = (op2) => {
-    let top = stackCp.pop();
-    switch (top) {
-      case '/':
-        return processStack() / op2;
-      case '*':
-        return processStack() * op2;
-      case '-':
-        return processStack() - op2;
-      case '+':
-        return processStack() + op2;
-      default:
-        return top;
-    }
-  }
-
-  let processResult = () => {
-    clearOpButtons();
-    if (!resetBuf) {
-      stack.push(buf);
-    }
-    stackCp = stack.map((v) => { return v; });
-    buf = processStack(stackCp.pop());
-    stack[0] = buf;
-    sep = 0;
-    resetBuf = true;
-    $buf.text(buf);
-  }
-
-  let processNum = (num) => {
-    if (resetBuf) {
-      resetBuf = false;
-      stack = [];
-      buf = 0;
-    }
-    if (buf === 0 && !sep) {
-      buf = num;
-    }
-    else {
-      if (sep != 0) {
-        buf += num * sep;
-        sep /= 10;
-      }
-      else {
-        buf *= 10;
-        buf += num;
-      }
-    }
-    $buf.text(buf);
-    $('td#6').text('C');
-  }
-
-  let processKey = ($btn) => {
-    switch (parseInt($btn.attr('id'))) {
-      case 6:
-        clearBuf();
-        stack = [];
-        $btn.text('AC');
-        break;
-      case 7:
-        buf *= -1;
-        $buf.text(buf);
-        break;
-      case 8:
-        buf *= prevNum();
-        buf /= 100;
-        $buf.text(buf);
-        break;
-      case 9:
-        processOp('/', $btn);
-        break;
-      case 16:
-        processNum(7);
-        break;
-      case 17:
-        processNum(8);
-        break;
-      case 18:
-        processNum(9);
-        break;
-      case 19:
-        processOp('*', $btn);
-        break;
-      case 26:
-        processNum(4);
-        break;
-      case 27:
-        processNum(5);
-        break;
-      case 28:
-        processNum(6);
-        break;
-      case 29:
-        processOp('-', $btn);
-        break;
-      case 36:
-        processNum(1);
-        break;
-      case 37:
-        processNum(2);
-        break;
-      case 38:
-        processNum(3);
-        break;
-      case 39:
-        processOp('+', $btn);
-        break;
-      case 46:
-        processNum(0);
-        break;
-
-      case 48:
-        sep = 0.1;
-        $buf.text($buf.text() + '.');
-        break;
-      case 49:
-        processResult();
-        break;
-    }
-  }
 
   let documentReady = () => {
-    $('.box').on('click', (event) => {
-      processKey($(event.target));
-    });
+    $('td.digits').on('click', processDigit);
+    $('td#ac').on('click', clearAll);
+    $('td#sign').on('click', changeSign);
+    $('td#percent').on('click', percent);
+    $('td#divide').on('click', {fn: divide}, setOperator);
+    $('td#multi').on('click', {fn: multi}, setOperator);
+    $('td#minus').on('click', {fn: minus}, setOperator);
+    $('td#plus').on('click', {fn: plus}, setOperator);
+    $('td#execute').on('click', execute);
+
+    clearAll();
   };
 
   $(documentReady);
 })();
 
+let modified = false;
+let newVal = false;
+
+let $buf = $('th#buf');
+let operand1 = undefined;
+let operator = undefined;
+let operand2 = undefined;
+
+let divide = (op1, op2) => {
+  return op1 / op2;
+}
+
+let multi = (op1, op2) => {
+  return op1 * op2;
+}
+
+let minus = (op1, op2) => {
+  return op1 - op2;
+}
+
+let plus = (op1, op2) => {
+  return op1 + op2;
+}
+
+let setOperator = (event) => {
+  $('td.ops').removeClass('pushed');
+  $(event.target).addClass('pushed');
+  if (operand1 === undefined) {
+    operand1 = parseFloat($buf.text());
+  }
+  newVal = true;
+  operator = event.data.fn;
+}
+
+let setModifyed = () => {
+  if (!modified) {
+    modified = true;
+    $('td#ac').text('C');
+  }
+}
+
+let setBuffer = (val) => {
+  let text = val.toString();
+  $buf.text(text);
+}
+
+let clearAll = () => {
+  if (modified) {
+    setBuffer('0');
+    $('td#ac').text('AC');
+    $('td.ops').removeClass('pushed');
+    operator = undefined;
+    operand2 = undefined;
+    modified = false;
+    newVal = false;
+  }
+  else {
+    operand1 = undefined;
+  }
+}
+
+let processDigit = (event) => {
+  let val = $(event.target).text();
+  let buf;
+  if (newVal) {
+    newVal = false;
+    setBuffer(buf = '0');
+  }
+  else {
+    buf = $buf.text();
+  }
+
+  if (val === '.') {
+    if (!~buf.indexOf('.')) {
+      setBuffer(buf + '.');
+    }
+  }
+  else if (buf === '0') {
+    setBuffer(val);
+  }
+  else {
+    setBuffer(buf + $(event.target).text());
+  }
+  setModifyed();
+}
+
+let changeSign = () => {
+  newVal = true;
+  let val = $buf.text();
+  if (val[0] === '-') {
+    setBuffer(val.substr(1));
+  }
+  else {
+    setBuffer('-' + val);
+  }
+}
+
+let percent = () => {
+  let part = parseFloat($buf.text()) / 100;
+  if (operand1 !== undefined) {
+    part *= operand1;
+  }
+  setBuffer(part);
+}
+
+let execute = () => {
+  if (operand1 === undefined ||
+      operator === undefined) {
+    return;
+  }
+  if (!newVal) {
+    operand2 = parseFloat($buf.text());
+  }
+  operand1 = operator(operand1, operand2);
+  setBuffer(operand1);
+  $('td.ops').removeClass('pushed');
+  newVal = true;
+}
